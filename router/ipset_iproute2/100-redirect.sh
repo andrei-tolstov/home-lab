1af9e5fafa9046f4076a4c74647c173662241665
andrei <andrei.tolstov@gmail.com>
2024-09-05 08:38:40 +0300

    v2

--- router/ipset_iproute2/100-redirect.sh
+++ router/ipset_iproute2/100-redirect.sh
@@ -17,13 +17,13 @@ fi
 
 # перенаправляю все днс 
 
-if [ -z "$(iptables-save 2>/dev/null | grep "udp \-\-dport 53 \-j DNAT")" ]; then
-   iptables -w -t nat -I PREROUTING -i br0 -p udp --dport 53 -j DNAT --to 192.168.50.1
-fi
+# if [ -z "$(iptables-save 2>/dev/null | grep "udp \-\-dport 53 \-j DNAT")" ]; then
+#    iptables -w -t nat -I PREROUTING -i br0 -p udp --dport 53 -j DNAT --to 192.168.50.1
+# fi
 
-if [ -z "$(iptables-save 2>/dev/null | grep "tcp \-\-dport 53 \-j DNAT")" ]; then
-   iptables -w -t nat -I PREROUTING -i br0 -p tcp --dport 53 -j DNAT --to 192.168.50.1
-fi
+# if [ -z "$(iptables-save 2>/dev/null | grep "tcp \-\-dport 53 \-j DNAT")" ]; then
+#    iptables -w -t nat -I PREROUTING -i br0 -p tcp --dport 53 -j DNAT --to 192.168.50.1
+# fi
 
 exit 0
 
