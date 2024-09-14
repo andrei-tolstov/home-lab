d82e4a0665518bbf5b52457c61f03aeb1255094f
andrei <andrei.tolstov@gmail.com>
2024-09-14 23:10:06 +0300

    fix_start.sh

--- remote-srv/amnezia-wg/build/start.sh
+++ remote-srv/amnezia-wg/build/start.sh
@@ -25,3 +25,4 @@ iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A POSTROUTING -s 10.8.1.0/24 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 10.8.1.0/24 -o eth1 -j MASQUERADE
 
+tail -f /dev/null
